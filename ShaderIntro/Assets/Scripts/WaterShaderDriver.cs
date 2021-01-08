b4d0a8d175f1e0cfa2e6eb6aef37a2b9fccdb997
paul_nasdalack <paul@indieburg.com>
2021-01-08 17:56:01 +0100

    Outline and Flask Shader

---
+++ ShaderIntro/Assets/Scripts/WaterShaderDriver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WaterShaderDriver : MonoBehaviour
+{
+    [SerializeField]
+    private Material m_material;
+    [SerializeField]
+    private MeshRenderer m_renderer;
+
+    private void Start()
+    {
+        if (m_material == null)
+        {
+            if (Application.isPlaying)
+            {
+                m_material = m_renderer.material;
+            }
+        }
+    }
+
+    void Update()
+    {
+        m_material.SetVector("WaterLevelOrigin", transform.position);
+        m_material.SetVector("WaterLevelNormal", transform.up);
+    }
+}
