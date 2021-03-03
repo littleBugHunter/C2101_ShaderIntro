f2116a358de54c0b13a86b35612328cc84bcbf81
NughtaedGames <34511481+NughtaedGames@users.noreply.github.com>
2021-03-03 19:41:26 +0100

    Adding new flower growing shader

---
+++ ShaderIntro/Assets/SpringAmountHandle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringAmountHandle : MonoBehaviour
+{
+    
+    public static int HandlePositionValue = Shader.PropertyToID("HandlePosition");
+
+    public Material material;
+    public Material material2;
+    
+    void Update()
+    {
+        material.SetVector(HandlePositionValue, this.transform.position);
+        material2.SetVector(HandlePositionValue, this.transform.position);
+    }
+}
